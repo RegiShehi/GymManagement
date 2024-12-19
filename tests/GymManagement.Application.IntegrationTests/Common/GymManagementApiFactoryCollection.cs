9903753872151451ee3fe0be93c52ec2f77f015b
Regi Shehi <regi_shehi@hotmail.com>
2024-12-19 12:12:03 +0100

    Add integration tests

---
+++ tests/GymManagement.Application.IntegrationTests/Common/GymManagementApiFactoryCollection.cs
@@ -0,0 +1,7 @@
+﻿namespace GymManagement.Application.IntegrationTests.Common;
+
+[CollectionDefinition(CollectionName)]
+public class GymManagementApiFactoryCollection : ICollectionFixture<GymManagementApiFactory>
+{
+    public const string CollectionName = "GymManagementApiFactoryCollection";
+}
